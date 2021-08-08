ec20913dc6a3c2a15e81c60ac7e63449be5a9687
Gabriel Barreto <barreto-gabriel@outlook.com>
2021-08-08 00:41:28 -0300

    fix: params types

--- src/Shared/Middlewares/exceptionHandler.middleware.ts
+++ src/Shared/Middlewares/exceptionHandler.middleware.ts
@@ -5,25 +5,22 @@ import forTerminal from 'youch-terminal'
 
 import { app } from '@/Config'
 
-import { ExpressReqWithError } from '@/Shared/Protocols'
 import { AppError, YupValidationError } from '@/Shared/Errors'
 
 export async function exceptionHandler(
   err: Error,
-  request: Request | ExpressReqWithError,
+  request: Request,
   response: Response,
   __: NextFunction
 ): Promise<any> {
-  const { error } = request as ExpressReqWithError
   if (app.env === 'development') {
     const youchErr = new Youch(err, request)
     const jsonErr = await youchErr.toJSON()
     console.log(err)
-    console.log(JSON.stringify(error, null, 2))
     console.log(forTerminal(jsonErr))
   }
-  if (error && error instanceof YupValidationError) {
-    return response.status(UNPROCESSABLE_ENTITY).json(error.toJSON())
+  if (err instanceof YupValidationError) {
+    return response.status(UNPROCESSABLE_ENTITY).json(err.toJSON())
   }
   if (err instanceof AppError) {
     return response.status(err.status).json(err.toJSON())
