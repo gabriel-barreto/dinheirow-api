b92c192cb681c40b0b6eb1b26b63fca0b341c2f2
Gabriel Barreto <barreto-gabriel@outlook.com>
2021-08-08 00:41:01 -0300

    feat: add ok method to responses helper

--- src/Shared/Helpers/response.helper.ts
+++ src/Shared/Helpers/response.helper.ts
@@ -1,7 +1,11 @@
-import { CREATED } from 'http-status'
+import { CREATED, OK } from 'http-status'
 
 export class Responses {
   public static created(body: Record<string, any>) {
     return { status: CREATED, body }
+  }
+
+  public static ok(body: Record<string, any>) {
+    return { status: OK, body }
   }
 }
