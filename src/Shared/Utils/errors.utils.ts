a40f57c216952bd3f4ba66b5ba3a6dbba1b32883
Gabriel Barreto <barreto-gabriel@outlook.com>
2021-08-08 00:42:48 -0300

    refactor: remove unused errors

--- src/Shared/Utils/errors.utils.ts
+++ src/Shared/Utils/errors.utils.ts
@@ -1,4 +1,4 @@
-import { BAD_REQUEST, FORBIDDEN, NOT_FOUND, UNAUTHORIZED } from 'http-status'
+import { BAD_REQUEST, NOT_FOUND, UNAUTHORIZED } from 'http-status'
 
 type Err = {
   status: number
@@ -12,11 +12,6 @@ export const $errors = {
     code: 'DUPLICATED',
     message: 'O documento informado já está registrado em nossa base de dados.'
   },
-  invalidFormat: {
-    status: BAD_REQUEST,
-    code: 'INVALID_FORMAT',
-    message: 'Os valores informados não correspondem aos tipos esperados.'
-  },
   notFound: {
     status: NOT_FOUND,
     code: 'NOT_FOUND',
@@ -28,41 +23,15 @@ export const $errors = {
     message:
       'O corpo recebido não contém todos os valores esperados ou não contém os tipos adequeados.'
   },
-  paramsValidationFails: {
-    status: BAD_REQUEST,
-    code: 'INVALID_PARAMS',
-    message:
-      'Os parâmetros recebidos não contém todos os valores esperados ou não contém os tipos adequeados.'
-  },
-  tokenNotFound: {
-    status: NOT_FOUND,
-    code: 'TOKEN_NOT_FOUND',
-    message: 'O token de autorização não foi informado'
-  },
   invalidToken: {
     status: UNAUTHORIZED,
     code: 'INVALID_TOKEN',
     message: 'O token informado é inválido!'
   },
   invalidPassword: {
-    status: UNAUTHORIZED,
+    status: BAD_REQUEST,
     code: 'INVALID_PASSWORD',
     message: 'A senha informada está incorreta!'
-  },
-  invalidAccess: {
-    status: FORBIDDEN,
-    code: 'FORBIDDEN',
-    message: 'O usuário autenticado não tem permissões suficientes!'
-  },
-  userStatusInvalid: {
-    status: FORBIDDEN,
-    code: 'USER_STATUS_INVALID',
-    message: 'O usuário não tem permissões para entrar.'
-  },
-  serviceUnavailable: {
-    status: 503,
-    code: 'SERVICE_UNAVAILABLE',
-    message: 'O serviço está indisponível no momento.'
   }
 }
 
