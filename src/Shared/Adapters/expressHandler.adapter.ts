dbd9047ce42ab8acf281226c0f77d6a20c440fa6
Gabriel Barreto <barreto-gabriel@outlook.com>
2021-08-09 01:52:07 -0300

    feat: add param to http req proitocol and express adapter

--- src/Shared/Adapters/expressHandler.adapter.ts
+++ src/Shared/Adapters/expressHandler.adapter.ts
@@ -4,7 +4,11 @@ import { Controller } from '@/Shared/Protocols'
 export class ExpressHandlerAdapter {
   public static adapt(controller: Controller) {
     return async function _handle(req: Request, res: Response) {
-      const httpReq = { body: req.body, headers: req.headers }
+      const httpReq = {
+        body: req.body,
+        headers: req.headers,
+        params: req.params
+      }
       const httpRes = await controller.handle(httpReq)
       return res.status(httpRes.status).send(httpRes.body)
     }
--- src/Shared/Protocols/HTTP.protocol.ts
+++ src/Shared/Protocols/HTTP.protocol.ts
@@ -2,7 +2,8 @@ import { User } from '@/Shared/Models'
 
 export type HttpReq<
   Body = Record<string, any>,
-  Headers = Record<string, any>
-> = { body: Body; headers: Headers }
+  Headers = Record<string, any>,
+  Params = Record<string, any>
+> = { body: Body; headers: Headers; params: Params }
 export type AuthHttpReq = HttpReq & { user: User }
 export type HttpRes<Body = Record<string, any>> = { status: number; body: Body }
